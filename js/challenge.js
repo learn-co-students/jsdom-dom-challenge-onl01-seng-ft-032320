fe42f49d1be45ada55920b719582463aa4c3cb02
Swishyfishie2 <pclandreiuk@gmail.com>
2020-06-22 23:36:52 +0100

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,56 @@
+const plus = document.querySelector("#plus")
+const minus = document.querySelector("#minus")
+const heart = document.querySelector("#heart")
+const counter = document.querySelector("#counter")
+const pause = document.querySelector("#pause")
+let newCount = 0
+
+
+
+myInterval = window.setInterval(increment, 1000)
+
+function increment() {
+    newCount += 1
+    counter.innerText = newCount
+}
+
+function decrement() {
+    newCount -= 1
+    counter.innerText = newCount
+}
+
+function like() {
+    document.querySelector(".likes").innerHTML += `<p>${newCount} has been liked!</p>`
+}
+
+function toggleTime(e) {
+
+
+    if (e.target.id == "resume") {
+        pause.id = "pause"
+        pause.innerText = "pause"
+        myInterval = window.setInterval(increment, 1000)
+    } else {
+        clearInterval(myInterval);
+        pause.id = "resume";
+        pause.innerText = "resume"
+
+    }
+}
+
+document.querySelector("#submit").addEventListener("click", (e) => {
+    e.preventDefault()
+    let input = document.querySelector("#comment-input")
+
+    let li = document.querySelector("#list")
+    const list = document.createElement("LI")
+
+    li.appendChild(list)
+    list.innerHTML = input.value
+    input.value = ""
+})
+
+pause.addEventListener("click", toggleTime)
+plus.addEventListener("click", increment)
+minus.addEventListener("click", decrement)
+heart.addEventListener("click", like)
