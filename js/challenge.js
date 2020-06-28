243640faaadba1cb97cba5ebf53ece293d7bd021
proiacm <proiacm@gmail.com>
2020-06-28 14:00:40 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const counter = document.getElementById('counter')
 
-let count = setInterval(function () {
+    let count = setInterval(function () {
     counter.innerText++; 
-}, 1000); 
+    }, 1000); 
 
 const minus = document.getElementById('minus')
 const plus = document.getElementById('plus')
@@ -14,30 +14,30 @@ const likes = document.querySelector('.likes')
 const submit = document.getElementById('submit')
 const list = document.querySelector('#list')
 
-minus.addEventListener("click", function(){
+    minus.addEventListener("click", function(){
     counter.innerText--;
-});
+    });
 
-plus.addEventListener("click", function(){
+    plus.addEventListener("click", function(){
     counter.innerText++;
-});
+    });
 
-heart.addEventListener("click", function(){
-    let c = document.getElementById(`${counter.innerText}`);
+    heart.addEventListener("click", function(){
+        let c = document.getElementById(`${counter.innerText}`);
         c ? c.children[0].innerText++ : 
         likes.innerHTML += `<li id=${counter.innerText}>${counter.innerText} was liked <span id=${counter.innerText}>1</span> time(s)</li>`
-});
+    });
 
-pause.addEventListener("click", function(){
-    if (pause.innerText == 'pause'){
+    pause.addEventListener("click", function(){
+        if (pause.innerText == 'pause'){
         pause.innerText = 'resume'
         clearInterval(count)
         plus.disabled = true;
         minus.disabled = true;
         heart.disabled = true; 
         submit.disabled = true;
-    }
-    else {
+        }
+        else {
         count = setInterval(function () {
             counter.innerText++; 
         }, 1000); 
@@ -46,13 +46,13 @@ pause.addEventListener("click", function(){
         minus.disabled = false;
         heart.disabled = false; 
         submit.disabled = false;
-    }
-});  
+        }
+    });  
 
-submit.addEventListener("click", function(e){
+    submit.addEventListener("click", function(e){
     e.preventDefault();
     let comment = document.querySelector("#comment-form > input[type=text]").value
     list.innerHTML += `<li>${comment}</li>`
     document.querySelector("#comment-form").reset();
-});
+    });
 });
